5a411d7d0be5349c5fdc4c14c43d09cf724420c0
mhrjnrahul <maharjanrahul58@gmail.com>
2024-07-02 16:46:47 +0545

    did ex 02

--- 02_repeatString/repeatString.spec.js
+++ 02_repeatString/repeatString.spec.js
@@ -4,10 +4,10 @@ describe('repeatString', () => {
   test('repeats the string', () => {
     expect(repeatString('hey', 3)).toEqual('heyheyhey');
   });
-  test.skip('repeats the string many times', () => {
+  test('repeats the string many times', () => {
     expect(repeatString('hello', 10)).toEqual('hellohellohellohellohellohellohellohellohellohello');
   });
-  test.skip('repeats the string 1 times', () => {
+  test('repeats the string 1 times', () => {
     expect(repeatString('hi', 1)).toEqual('hi');
   });
   test.skip('repeats the string 0 times', () => {
